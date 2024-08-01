ca8f9a7e39722183ecf0ef3e43abbfc893187ba4
glenrosethulisile97 <146944842+glenrosethulisile97@users.noreply.github.com>
2024-08-01 15:21:15 +0200

    today,s work

---
+++ config.py
@@ -0,0 +1,4 @@
+class config:
+  SECRET_KEY = 'your_secret_key'
+  JWT_SECRET_KEY = 'your_jwt_secret_key'
+  MONGO_URI="mongodb+srv://thulisilemolefe05:<password>@flask23.vfomws5.mongodb.net/?retryWrites=true&w=majority&appName=Flask23"
